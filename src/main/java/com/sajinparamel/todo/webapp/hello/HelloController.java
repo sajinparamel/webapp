2035dc2caf149e4d9a0eb579b827dfbebaf28d4e
Sajin Paramel <sajinparamel@gmail.com>
2022-12-02 19:18:48 +0530

    Connecting to a JSP Page

--- src/main/java/com/sajinparamel/todo/webapp/hello/HelloController.java
+++ src/main/java/com/sajinparamel/todo/webapp/hello/HelloController.java
@@ -7,9 +7,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-@RequestMapping("hello-map")
-@ResponseBody
-public String sayHello() {
-	return "Hello Good Day for Programming";
-}
+    @RequestMapping("hello-map")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello Good Day for Programming";
+    }
+
+
+    @RequestMapping("hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("HTML Sample Page");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("HTML Sample Body");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
+
+    @RequestMapping("hello-jsp")
+    public String sayHelloJSP() {
+        return "SayHello";
+    }
 }
