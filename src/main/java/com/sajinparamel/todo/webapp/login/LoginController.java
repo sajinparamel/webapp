6310890e5388f97dedc9073865fbab24363ca040
Sajin Paramel <sajinparamel@gmail.com>
2022-12-02 19:37:17 +0530

    Login jsp logic added

---
+++ src/main/java/com/sajinparamel/todo/webapp/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.sajinparamel.todo.webapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	   @RequestMapping("login")
+	    public String goToLoginJSP() {
+	        return "login";
+	    }
+}
